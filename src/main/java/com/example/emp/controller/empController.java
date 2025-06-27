b8c0cd8e9f189f1a737e44cb5d9a0ae28641985e
KBARATH13 <barath13015@gmail.com>
2025-06-27 22:40:42 +0530

    changed frontend url

--- src/main/java/com/example/emp/controller/empController.java
+++ src/main/java/com/example/emp/controller/empController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"})
+@CrossOrigin(origins = {"https://projectfrontendemp.netlify.app/"})
 @RequestMapping("/employees")
 public class empController {
 
