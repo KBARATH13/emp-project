90efc9555d279638b3d9db114d7bd5c42ddcf5d6
KBARATH13 <barath13015@gmail.com>
2025-06-28 11:29:03 +0530

    setup made for deploying in render

--- src/main/java/com/example/emp/controller/empController.java
+++ src/main/java/com/example/emp/controller/empController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"https://projectfrontendemp.netlify.app/"})
+@CrossOrigin(origins = {"https://projectfrontendemp.netlify.app"})
 @RequestMapping("/employees")
 public class empController {
 
