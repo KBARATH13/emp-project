d4632d7828e06bf5c7ac7c53ac4b59f81ed6773f
KBARATH13 <barath13015@gmail.com>
2025-07-21 21:47:21 +0530

    upload

--- src/main/java/com/example/emp/controller/empController.java
+++ src/main/java/com/example/emp/controller/empController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = {"https://projectfrontendemp.netlify.app"})
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500"})
 @RequestMapping("/employees")
 public class empController {
 
